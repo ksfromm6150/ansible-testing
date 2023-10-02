e8254487a3816c2ee65f1322b7b12e9129dc3a3f
ksfromm6150 <kyle@particle41.com>
2023-10-02 11:22:26 -0500

    Added Terraform folder structure and template files

--- terraform/envs/stage/main.tf
+++ terraform/envs/stage/main.tf
@@ -4,7 +4,7 @@ module "functions_stage" {
     vpc_subnet_ids         = var.vpc_subnet_ids
     vpc_security_group_ids = var.vpc_security_group_ids
 
-image_test = var.image_test
-image_TEST2 = var.image_TEST2
+  image_test = var.image_test
+  image_TEST2 = var.image_TEST2
 
 }
