442a026b6776e694b95550a51e930ba70bd4dbb8
ksfromm6150 <kyle@particle41.com>
2023-10-02 10:14:17 -0500

    Added Terraform folder structure and template files

--- terraform/envs/stage/main.tf
+++ terraform/envs/stage/main.tf
@@ -1,3 +1,6 @@
 module "functions_stage" {
     source = "../../modules/functions"
+
+    vpc_subnet_ids         = var.vpc_subnet_ids
+  vpc_security_group_ids = var.vpc_security_group_ids
 }
--- terraform/envs/stage/variables.tf
+++ terraform/envs/stage/variables.tf
@@ -2,3 +2,54 @@ variable "environment" {
   description = "The environment: stage"
   default     = "stage"
 }
+
+variable "vpc_subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs"
+  default     = null
+}
+
+variable "vpc_security_group_ids" {
+  type        = list(string)
+  description = "List of security group IDs"
+  default     = null
+}
+
+
+/* images */
+
+variable "image_collectrenewals" {
+  type        = string
+  description = "ECR image URI for the named lambda"
+}
+
+variable "image_customerlistener" {
+  type        = string
+  description = "ECR image URI for the named lambda"
+}
+
+variable "image_gatherrequests" {
+  type        = string
+  description = "ECR image URI for the named lambda"
+}
+
+variable "image_orderlistener" {
+  type        = string
+  description = "ECR image URI for the named lambda"
+}
+
+variable "image_quotedelivery" {
+  type        = string
+  description = "ECR image URI for the named lambda"
+}
+
+variable "image_vendorlistener" {
+  type        = string
+  description = "ECR image URI for the named lambda"
+}
+
+variable "image_workitemlistener" {
+  type        = string
+  description = "ECR image URI for the named lambda"
+}
+
