ec016108007c831d1151d4fd808d986a29eb5ff0
ksfromm6150 <kyle@particle41.com>
2023-10-02 10:16:55 -0500

    Added Terraform folder structure and template files

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -2,5 +2,5 @@ module "functions_dev" {
     source = "../../modules/functions"
 
     vpc_subnet_ids         = var.vpc_subnet_ids
-  vpc_security_group_ids = var.vpc_security_group_ids
+    vpc_security_group_ids = var.vpc_security_group_ids
 }
--- terraform/envs/stage/main.tf
+++ terraform/envs/stage/main.tf
@@ -2,5 +2,5 @@ module "functions_stage" {
     source = "../../modules/functions"
 
     vpc_subnet_ids         = var.vpc_subnet_ids
-  vpc_security_group_ids = var.vpc_security_group_ids
+    vpc_security_group_ids = var.vpc_security_group_ids
 }
